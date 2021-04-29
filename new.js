d1ccc3a915d12a6fa1a3916560a437a5eee4ec28
luise-go <luise.goede@yahoo.de>
2021-04-29 13:41:54 +0200

    link to local storage

--- new.js
+++ new.js
@@ -6,6 +6,43 @@ formElement.onsubmit = function (event) {
   const checkedDateInput = document.querySelector(
     ".radio-group__input:checked"
   );
+  if (!taskNameInput.value && !checkedDateInput) {
+    alert("Please enter task and choose date");
+    return;
+  } else if (!taskNameInput.value) {
+    alert("Plase enter task");
+    return;
+  } else if (!checkedDateInput) {
+    alert("Please choose date");
+    return;
+  }
 
-  console.log(taskNameInput.value, checkedDateInput.value);
+  const task = {
+    name: taskNameInput.value,
+    date: checkedDateInput.value,
+  };
+
+  //   // console.log(newTask);
+  //   const taskJSON = JSON.stringify(task);
+  //   localStorage.setItem("taskList", taskJSON);
+  //
+  // get existing taskList (localStorage) and parse to array + if storage empty e.g. parse for the first time
+
+  let oldTaskListJSON = localStorage.getItem("taskList");
+  if (oldTaskListJSON === null) {
+    oldTaskListJSON = "[]";
+  }
+  const taskListParse = JSON.parse(oldTaskListJSON);
+
+  // add item to array / extend array with item
+  taskListParse.push(task);
+
+  // signify and set to local storage
+  const taskListJSON = JSON.stringify(taskListParse);
+  localStorage.setItem("taskList", taskListJSON);
+
+  function goToPage(href) {
+    location.href = href;
+  }
+  goToPage("/dashboard.html");
 };
